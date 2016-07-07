2ff43764ef991e1dcfa5a5b85d92433c8bd40bfc
Andrew McCauley <andrew.mccauley@shopify.com>
2016-07-07 18:34:26 -0400

    So it begins

---
+++ Gemfile
@@ -0,0 +1,4 @@
+gem 'sinatra'
+gem 'httparty'
+gem 'shopify_api', '~> 4.0.7'
+gem 'dotenv'
--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-puts hello world
+require 'dotenv'
+require 'sinatra'
+require 'shopify_api'
+require 'httparty'
+
+class GoodieBasket < Sinatra::Base
+
+	def init
+	end
+end
